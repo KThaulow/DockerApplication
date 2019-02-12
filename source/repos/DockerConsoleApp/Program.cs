b2d21833b1f91978b6a894768d54dea9facea72a
Kristian Thaulow <kristian1990@gmail.com>
2019-02-12 18:35:25 +0100

    Initial commit

---
+++ source/repos/DockerConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DockerConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+
+			Console.WriteLine("Kristian her. Jeg siger hej til  smukke Liv!");
+
+
+
+			Console.WriteLine("Liv her. Du er for sej!");
+
+			Console.WriteLine("Kristian her. Tak!");
+		}
+	}
+}
